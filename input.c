4e0a54081c4a06371f8049634e3f405d1e947ead
koustav-B <147908224+koustav-B@users.noreply.github.com>
2023-10-18 15:23:34 +0530

    Add files via upload

---
+++ input.c
@@ -0,0 +1,161 @@
+#include <stdio.h>
+
+#define MAX 100 // Define a maximum value
+
+// This is a single-line comment
+
+int main() {
+    int sum = 0;
+    /*
+    This is a multi-line comment.
+    It can s#include <stdio.h>
+
+/*
+   This is a simple C program that
+   prints "Hello, World!" to the
+   console.
+*/
+
+int main() {
+    // This is the main function.
+    printf("Hello, World!\n");  // Print the message
+    return 0;  // Return 0 to indicate success
+}
+
+/*
+   These are additional comment lines
+   to reach a total of 100 comment lines.
+
+   You can add more comments here if needed.
+   Each line is counted as a comment line.
+   Comments are for documentation and
+   explanations in your code.
+*/
+
+// Additional comment line 1
+// Additional comment line 2
+// Additional comment line 3
+// Additional comment line 4
+// Additional comment line 5
+// Additional comment line 6
+// Additional comment line 7
+// Additional comment line 8
+// Additional comment line 9
+// Additional comment line 10
+// Additional comment line 11
+// Additional comment line 12
+// Additional comment line 13
+// Additional comment line 14
+// Additional comment line 15
+// Additional comment line 16
+// Additional comment line 17
+// Additional comment line 18
+// Additional comment line 19
+// Additional comment line 20
+// Additional comment line 21
+// Additional comment line 22
+// Additional comment line 23
+// Additional comment line 24
+// Additional comment line 25
+// Additional comment line 26
+// Additional comment line 27
+// Additional comment line 28
+// Additional comment line 29
+// Additional comment line 30
+// Additional comment line 31
+// Additional comment line 32
+// Additional comment line 33
+// Additional comment line 34
+// Additional comment line 35
+// Additional comment line 36
+// Additional comment line 37
+// Additional comment line 38
+// Additional comment line 39
+// Additional comment line 40
+// Additional comment line 41
+// Additional comment line 42
+// Additional comment line 43
+// Additional comment line 44
+// Additional comment line 45
+// Additional comment line 46
+// Additional comment line 47
+// Additional comment line 48
+// Additional comment line 49
+// Additional comment line 50
+// Additional comment line 51
+// Additional comment line 52
+// Additional comment line 53
+// Additional comment line 54
+// Additional comment line 55
+// Additional comment line 56
+// Additional comment line 57
+// Additional comment line 58
+// Additional comment line 59
+// Additional comment line 60
+// Additional comment line 61
+// Additional comment line 62
+// Additional comment line 63
+// Additional comment line 64
+// Additional comment line 65
+// Additional comment line 66
+// Additional comment line 67
+// Additional comment line 68
+// Additional comment line 69
+// Additional comment line 70
+// Additional comment line 71
+// Additional comment line 72
+// Additional comment line 73
+// Additional comment line 74
+// Additional comment line 75
+// Additional comment line 76
+// Additional comment line 77
+// Additional comment line 78
+// Additional comment line 79
+// Additional comment line 80
+// Additional comment line 81
+// Additional comment line 82
+// Additional comment line 83
+// Additional comment line 84
+// Additional comment line 85
+// Additional comment line 86
+// Additional comment line 87
+// Additional comment line 88
+// Additional comment line 89
+// Additional comment line 90
+// Additional comment line 91
+// Additional comment line 92
+// Additional comment line 93
+// Additional comment line 94
+// Additional comment line 95
+// Additional comment line 96
+// Additional comment line 97
+// Additional comment line 98
+// Additional comment line 99
+// Additional comment line 100
+//pan across several lines.
+    
+    for (int i = 1; i <= MAX; i++) {
+        sum += i; // Add the numbers from 1 to MAX
+    }
+
+    printf("The sum of numbers from 1 to %d is %d\n", MAX, sum);
+
+    /*
+    Here's some more code:
+    int x = 5; // An inline comment
+    int y = x * 2;
+    */
+
+    return 0; // Exit with status 0
+}
+
+// Another single-line comment
+
+#ifndef DEBUG
+#define LOG(message) printf("Message: %s\n", message)
+#endif
+
+int some_function(int x, int y) {
+    return x + y; // Add x and y
+}
+
